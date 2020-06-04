17208e95dc41a02c029d7f89ec3a983b5dd2e2b6
Yorick Demichelis <yorick.demichelis@techholding.co>
2020-06-04 16:05:15 -0700

    feat: create module for Azure API Management resource

---
+++ main.tf
@@ -0,0 +1,42 @@
+resource "azurerm_api_management_api" "api" {
+  name                = var.name
+  resource_group_name = var.resource_group.name
+  api_management_name = var.apimgt.name
+  revision            = var.revision
+  display_name        = var.display_name
+  path                = var.path
+  protocols           = ["https"]
+
+  subscription_key_parameter_names {
+    header = var.subscription_key_parameters.header
+    query  = var.subscription_key_parameters.query
+  }
+}
+
+resource "azurerm_api_management_api_operation" "api" {
+  operation_id        = var.method_id
+  api_name            = azurerm_api_management_api.api.name
+  api_management_name = var.apimgt.name
+  resource_group_name = var.resource_group.name
+  display_name        = var.method_name
+  method              = var.method
+  url_template        = var.url
+}
+
+resource "azurerm_api_management_api_operation_policy" "api" {
+  api_name            = azurerm_api_management_api.api.name
+  api_management_name = var.apimgt.name
+  resource_group_name = var.resource_group.name
+  operation_id        = azurerm_api_management_api_operation.api.operation_id
+
+  xml_content = <<XML
+<policies>
+  <inbound>
+    <set-backend-service base-url="${var.backend.url}" />
+  </inbound>
+  <backend>
+    <forward-request />
+  </backend>
+</policies>
+XML
+}
---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "name" {}
+variable "resource_group" {
+  default = { name = "" }
+}
+variable "apimgt" {
+  default = { name = "" }
+}
+variable "revision" {
+  default = "1"
+}
+variable "display_name" {}
+variable "path" {}
+variable "subscription_key_parameters" {
+  default = { header = "API-Key" query = "key" }
+}
+variable "method_id" {}
+variable "method_name" {}
+variable "method" { default = "GET" }
+variable "url" { default = "/*"}
+variable "backend" {
+  default = { url = "" }
+}
